f579b9cb721bdfd8d43827cf86594187d8e6b58f
mferlita <mferlitapro@gmail.com>
2026-07-28 00:19:28 -0400

    ch_1_calc.rb

---
+++ ch_2.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: false
+
+# printing strings
+puts 'PRINTING STRINGS'
+puts 'Hello, world!'
+puts ''
+puts 'Good-bye.'
+puts ''
+
+# string arithmetic
+puts 'STRING ARITHMETIC'
+puts 'I like' + 'apple pie.'
+puts 'I like ' + 'apple pie.'
+puts 'I like' + ' apple pie.'
+puts 'blink ' * 4
+puts ''
+
+# 12 vs '12'
+puts "12 VS '12'"
+puts  12  +  12
+puts '12' + '12'
+puts '12  +  12'
+puts  2  *  5
+puts '2' *  5
+puts '2  *  5'
+puts ''
+
+#problems
+puts 'PROBLEMS'
+=begin
+These don't work:
+  puts '12' + 12
+  puts '2' * '5'
+  puts 'Betty' + 12
+  puts 'Fred' * 'John'
+  puts 'You're swell!'
+=end
+puts '--Apostrophes and backslashes'
+puts 'You\'re swell!'
+puts 'backslash at the end of a string:  \\'
+puts 'up\\down'
+puts 'up\down'
+
