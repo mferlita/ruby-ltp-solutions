5bb9da248fcdf24a81d7a2a5f56332c037eef461
mferlita <mferlitapro@gmail.com>
2026-08-10 21:49:30 -0400

    Realized a better way to do this.

--- ch_6_leap_years.rb
+++ ch_6_leap_years.rb
@@ -1,18 +1,37 @@
 # frozen_string_literal: false
 
+# Commented out is my original program. It works fine, but there's a bit of redundancy.
+#
+# puts 'Please enter a starting year (the earliest year in the range).'
+# start_year = gets.to_i
+# puts 'Please enter an ending year (the latest year in the range).'
+# end_year = gets.to_i
+# puts 'The leap years are:'
+#
+# while start_year <= end_year
+#   if (start_year % 400).zero?
+#     puts start_year
+#   elsif (start_year % 100).zero?
+#     puts ''
+#   elsif (start_year % 4).zero?
+#     puts start_year
+#   end
+#   start_year += 1
+# end
+
+# Revamped
 puts 'Please enter a starting year (the earliest year in the range).'
 start_year = gets.to_i
 puts 'Please enter an ending year (the latest year in the range).'
 end_year = gets.to_i
 puts 'The leap years are:'
+year = start_year
 
-while start_year <= end_year
-  if (start_year % 400).zero?
-    puts start_year
-  elsif (start_year % 100).zero?
-    puts ''
-  elsif (start_year % 4).zero?
-    puts start_year
+while year <= end_year
+  if year % 4 == 0
+    if year % 100 != 0 || year % 400 == 0
+    puts year
+    end
   end
-  start_year += 1
+  year += 1
 end
