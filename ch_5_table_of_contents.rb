9f531076ed717a92d5cf5cfc0838c9aefc6ba5c1
mferlita <mferlitapro@gmail.com>
2026-08-14 00:38:13 -0400

    adding a new line between title and chapters

--- ch_5_table_of_contents.rb
+++ ch_5_table_of_contents.rb
@@ -10,6 +10,7 @@ ch2pg = 'page 5'
 ch3pg = 'page 9'
 
 puts(title.center(line_width))
+puts
 puts(ch1.ljust(line_width/2) + ch1pg.rjust(line_width/2))
 puts(ch2.ljust(line_width/2) + ch2pg.rjust(line_width/2))
 puts(ch3.ljust(line_width/2) + ch3pg.rjust(line_width/2))
