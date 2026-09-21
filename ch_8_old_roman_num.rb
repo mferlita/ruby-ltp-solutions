8610f06b14d8a3dbf917766e734eea6d2feb31da
mferlita <95393136+mferlita@users.noreply.github.com>
2026-09-21 02:05:08 -0400

    going to attempt to solve myself

--- ch_8_old_roman_num.rb
+++ ch_8_old_roman_num.rb
@@ -6,16 +6,6 @@
 # Integer division and modulus methods.
 
 def old_roman_numeral(num)
-  raise "Must use positive integer" if num <= 0
-  roman = ""
-
-  roman = roman + "M" * (num / 1000)
-  roman = roman + "D" * ((num % 1000) / 500)
-  roman = roman + "C" * ((num % 500) / 100)
-  roman = roman + "L" * ((num % 100) / 50)
-  roman = roman + "X" * ((num % 50) / 10)
-  roman = roman + "V" * ((num % 10) / 5)
-  roman = roman + "I" * ((num % 5) / 1)
 
   return roman
 end
